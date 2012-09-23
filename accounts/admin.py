9851c86027d0758aaa357ab6eacd09cb2033600e
Eugene <yeo.eugene.oey@gmail.com>
2012-09-23 23:21:18 +0800

    Improve UserAdmin Page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,17 @@
-from accounts.models import UserProfile
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
+from accounts.models import Company
 
-admin.site.register(UserProfile)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(UserAdmin):
+    inlines = [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Company)
